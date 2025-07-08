095b2abb50d52d4f8c2ecc7a0b4634fc3ac39826
ladymary99 <marycherie999@gmail.com>
2025-07-08 22:22:41 +0430

    #4

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import IngredientsList from "./components/IngredientsList";
+import ClaudeRecipe from "./components/ClaudeRecipe";
+import { getRecipeFromChefClaude, getRecipeFromMistral } from "./ai";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function Main() {
+  const [ingredients, setIngredients] = React.useState([
+    "chicken",
+    "all the main spices",
+    "corn",
+    "heavy cream",
+    "pasta",
+  ]);
+  const [recipe, setRecipe] = React.useState("");
+
+  async function getRecipe() {
+    const recipeMarkdown = await getRecipeFromChefClaude(ingredients);
+    setRecipe(recipeMarkdown);
+  }
+
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
+  return (
+    <main>
+      <form action={addIngredient} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+
+      {ingredients.length > 0 && (
+        <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
+      )}
+
+      {recipe && <ClaudeRecipe recipe={recipe} />}
+    </main>
+  );
+}
